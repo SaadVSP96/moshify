579db7d8c6d87b8538dd954292fe5a47bbb6a175
SaadVSP96 <saadmasood192@gmail.com>
2024-07-16 17:45:09 +0500

    22. Built the footer block

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  })
-);
+document.addEventListener("DOMContentLoaded", function () {
+  const toggler = document.querySelector(".nav__toggler");
+  toggler.addEventListener("click", function () {
+    const nav = this.closest(".nav");
+    nav.classList.toggle("collapsible--expanded");
+    document
+      .getElementsByClassName("collapsible__content")[0]
+      .classList.toggle("collapsible_rand");
+  });
+});
